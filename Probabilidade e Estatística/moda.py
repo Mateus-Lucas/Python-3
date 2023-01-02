f931cfd004b06875948b21a58e8a0f4c5abcc9b4
Mateus Lucas <mateus.lucas.ferreira.07@gmail.com>
2023-01-02 12:41:57 -0300

    Create moda.py

---
+++ Probabilidade e Estatística/moda.py
@@ -0,0 +1,14 @@
+#Questão 2 - Moda
+
+#Função:
+def moda(escala):
+  freq_esc = list() #inicializando lista de frequência
+  for i in escala:
+    freq_esc.append(escala.count(i)) #Lista de frequência
+  freq_max = freq_esc.index(max(freq_esc)) #frequência máxima
+  return escala[freq_max] #valor da moda 
+  
+escala = [67, 75, 63, 72, 77, 78, 81, 77, 80]
+import statistics 
+print('b)Moda:', moda(escala))
+print('Moda:', statistics.mode(escala))
