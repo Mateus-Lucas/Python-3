c32799d65486b91e0e8a3c0a0aeb18ea4a277f6d
Mateus Lucas <mateus.lucas.ferreira.07@gmail.com>
2023-01-03 10:12:01 -0300

    Create mediana.py

---
+++ Probabilidade e Estatística/mediana.py
@@ -0,0 +1,19 @@
+#Questão 2 - Mediana 
+
+#Função:
+def mediana(escala):
+  escala.sort() #ordenando a escala
+  n = len(escala) #contagem dos elemento
+  if n%2==0: # n é par
+    pos1 = n/2 - 1 
+    pos2 = n/2
+    media = (escala[pos1] + escala[pos2])/2
+  else: # n é impar
+    pos = (n-1)/2
+    media = escala[int(pos)]
+  return media
+
+import statistics
+escala = [67, 75, 63, 72, 77, 78, 81, 77, 80]
+print('c)Mediana:', mediana(escala))
+print('Mediana biblioteca:', statistics.median(escala))
