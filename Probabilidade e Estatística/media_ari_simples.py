3c90b8bcdbebf027348f3370e5408962cfd9dd82
Mateus Lucas <mateus.lucas.ferreira.07@gmail.com>
2023-01-04 20:27:03 -0300

    Create media_ari_simples.py

---
+++ Probabilidade e Estatística/media_ari_simples.py
@@ -0,0 +1,15 @@
+#Questão 1 - Média aritmética simples
+
+#Função:
+def media_ari(consumo):
+  num = 0 
+  n = len(consumo) #Contagem do número de elementos
+  for i in consumo:
+    num = num + i
+  media = num/n #calculo da média aritmética
+  return media
+
+consumo = [10, 13, 17, 9,  8, 11, 13, 7]
+import statistics 
+print('a)Média aritimética:', media_ari(consumo))
+print('Média aritimética da biblioteca:', statistics.mean(consumo))
